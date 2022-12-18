a8710ba666d5e94dc6a7a9d5cc348904e8a900ec
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2022-12-19 00:28:11 +0100

    Dao interface methods

--- src/main/java/ba/unsa/etf/rpr/Dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/Dao.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.Dao;
 
+import java.util.List;
+
 public interface Dao<T> {
+
+    T getById(int id);
+    T add(T item);
+    T update(T item);
+    void delete(int id);
+    List<T> getAll();
+
+
 }
