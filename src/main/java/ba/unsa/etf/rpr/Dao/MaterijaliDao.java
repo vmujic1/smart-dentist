789e2e75a7b950beea1fe1c7a7977bd69b2c0773
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2022-12-19 00:47:40 +0100

    searchByName method for MaterijaliDao interface

--- src/main/java/ba/unsa/etf/rpr/Dao/MaterijaliDao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/MaterijaliDao.java
@@ -1,6 +1,12 @@
 package ba.unsa.etf.rpr.Dao;
 
+import ba.unsa.etf.rpr.domain.Materijali;
+
+import java.util.List;
+
 public interface MaterijaliDao {
+
+    List<Materijali> searchByName(String ime);
 
     
 }
