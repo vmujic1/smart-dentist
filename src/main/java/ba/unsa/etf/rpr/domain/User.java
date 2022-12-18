800f9f75299856fe5b7002ab66724914ce1dc76e
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2022-12-18 23:53:01 +0100

    hashCode and equals for User class

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class User {
 
     private int idUser;
@@ -47,5 +49,18 @@ public class User {
 
     public void setLozinka(String lozinka) {
         this.lozinka = lozinka;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user1 = (User) o;
+        return idUser == user1.idUser && Objects.equals(imeOrdinacije, user1.imeOrdinacije) && Objects.equals(user, user1.user) && Objects.equals(lozinka, user1.lozinka);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idUser, imeOrdinacije, user, lozinka);
     }
 }
