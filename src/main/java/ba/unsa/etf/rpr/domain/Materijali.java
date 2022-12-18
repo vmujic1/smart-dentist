e167db2e76d263bacf6003ea82a3db541034051d
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2022-12-18 23:50:19 +0100

    hashCode and quals for materijali class

--- src/main/java/ba/unsa/etf/rpr/domain/Materijali.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Materijali.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Materijali {
 
     private int id;
@@ -37,5 +39,18 @@ public class Materijali {
 
     public void setKoličina(int količina) {
         this.količina = količina;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Materijali that = (Materijali) o;
+        return id == that.id && količina == that.količina && Objects.equals(naziv, that.naziv);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, naziv, količina);
     }
 }
