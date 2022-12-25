3852744b50357d50a24796c48b64aad1a002061e
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2022-12-26 00:26:43 +0100

    First controller

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 /**
  * author Vedran Mujic
@@ -14,8 +17,10 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Hello, World!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Get starting");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        //primaryStage.getIcons().add(new Image(""));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
     public static void main(String[] args) {
