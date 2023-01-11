20bc7c50949d2575b5294665f47c115135e0194e
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-01-11 19:07:33 +0100

    db.properties added

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
@@ -14,17 +13,18 @@ import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
  *
  */
 public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Get starting");
-        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        //primaryStage.getIcons().add(new Image(""));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-    }
+
     public static void main(String[] args) {
         launch(args);
     }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root  = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage.setTitle("SmartDentist");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+
 }
 
---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginControler.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.controllers;
+
+import java.awt.event.ActionEvent;
+
+public class LoginControler {
+
+    public void registrujSeAction(ActionEvent actionEvent){
+
+    }
+}
