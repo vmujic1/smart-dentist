19298f8f71ae763649eac8c36e88312ed8c22136
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-01-11 19:32:14 +0100

    Methods in UserDao

--- src/main/java/ba/unsa/etf/rpr/Dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/UserDao.java
@@ -2,7 +2,27 @@ package ba.unsa.etf.rpr.Dao;
 
 import ba.unsa.etf.rpr.domain.User;
 
+/**
+ * Dao interface for User domain bean
+ *
+ * @author Vedran Mujic
+ */
 public interface UserDao extends Dao<User>{
 
-    String searchByName(String text);
+
+    /**
+     * Returns user with given username.
+     *
+     * @param text that represent username.
+     * @return User object
+     */
+    User searchByUsername(String text);
+
+    /**
+     * Returns user with given Ordination name.
+     *
+     * @param text that represent Ordination name.
+     * @return User object
+     */
+    User searchByOrdinationUsername(String text);
 }
