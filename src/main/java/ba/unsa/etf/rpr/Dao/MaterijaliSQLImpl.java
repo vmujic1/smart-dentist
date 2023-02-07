6951635926a4fc9fc6b3a4adb39acd8fcbbfd3ea
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-07 21:24:45 +0100

    MaterijaliSQLImpl class

---
+++ src/main/java/ba/unsa/etf/rpr/Dao/MaterijaliSQLImpl.java
@@ -0,0 +1,37 @@
+package ba.unsa.etf.rpr.Dao;
+
+import ba.unsa.etf.rpr.domain.Materijali;
+
+import java.util.List;
+
+public class MaterijaliSQLImpl implements MaterijaliDao{
+    @Override
+    public Materijali getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Materijali add(Materijali item) {
+        return null;
+    }
+
+    @Override
+    public Materijali update(Materijali item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Materijali> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Materijali> searchByName(String ime) {
+        return null;
+    }
+}
