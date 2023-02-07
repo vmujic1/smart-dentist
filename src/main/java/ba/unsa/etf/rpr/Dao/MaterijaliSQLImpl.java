061b9f201ad72e1c7298f07045a7f87290c12067
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-07 21:26:20 +0100

    MaterijaliSQLImpl constructor

--- src/main/java/ba/unsa/etf/rpr/Dao/MaterijaliSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/MaterijaliSQLImpl.java
@@ -2,9 +2,31 @@ package ba.unsa.etf.rpr.Dao;
 
 import ba.unsa.etf.rpr.domain.Materijali;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 public class MaterijaliSQLImpl implements MaterijaliDao{
+
+    public Connection connection;
+
+    public MaterijaliSQLImpl() {
+        try{
+            FileReader reader = new FileReader("db.properties");
+            Properties p = new Properties();
+            p.load(reader);
+            this.connection = DriverManager.getConnection(p.getProperty("db.url"),p.getProperty("db.username"),p.getProperty("db.password"));
+        } catch(SQLException | IOException e){
+            System.out.println("X");
+            e.printStackTrace();
+        }
+    }
+
+
     @Override
     public Materijali getById(int id) {
         return null;
