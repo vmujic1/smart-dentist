732946a4bd7a9f27cecec20adc489170bb0f5ccc
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-10 00:16:48 +0100

    AbstractDao createConnection impl

--- src/main/java/ba/unsa/etf/rpr/Dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/AbstractDao.java
@@ -3,6 +3,9 @@ package ba.unsa.etf.rpr.Dao;
 import ba.unsa.etf.rpr.domain.Idable;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public abstract class AbstractDao<T extends Idable> implements Dao<T> {
     private  static Connection connection = null;
@@ -11,4 +14,38 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T> {
     public AbstractDao(String tableName){
         this.tableName = tableName;
     }
+
+    private static void createConnection(){
+        if(AbstractDao.connection == null){
+            try{
+                Properties p = new Properties();
+                p.load(ClassLoader.getSystemResource("application.properties").openStream());
+
+                String url = p.getProperty("db.connencion_string");
+                String username = p.getProperty("db.username");
+                String password = p.getProperty("db.password");
+
+                AbstractDao.connection = DriverManager.getConnection(url,username,password);
+            }catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                Runtime.getRuntime().addShutdownHook(new Thread(){
+                    @Override
+                    public void run(){
+                        try{
+                            connection.close();
+                        }catch (SQLException e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+
+
+
+
+
 }
