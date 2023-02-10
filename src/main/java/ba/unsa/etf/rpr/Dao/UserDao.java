700d8605edb593e2283f1cba4d2a275084bdb6a1
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-10 01:03:45 +0100

    UserDao refactoring

--- src/main/java/ba/unsa/etf/rpr/Dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/UserDao.java
@@ -9,20 +9,4 @@ import ba.unsa.etf.rpr.domain.User;
  */
 public interface UserDao extends Dao<User>{
 
-
-    /**
-     * Returns user with given username.
-     *
-     * @param text that represent username.
-     * @return User object
-     */
-    User searchByUsername(String text);
-
-    /**
-     * Returns user with given Ordination name.
-     *
-     * @param text that represent Ordination name.
-     * @return User object
-     */
-    User searchByOrdinationUsername(String text);
 }
