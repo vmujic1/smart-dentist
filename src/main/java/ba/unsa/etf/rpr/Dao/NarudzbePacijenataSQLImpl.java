11cae1559723755768f78ca67d9b2fd363df9381
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-10 01:37:18 +0100

    NarudzbePacijenataSQLImpl row2object method impl

--- src/main/java/ba/unsa/etf/rpr/Dao/NarudzbePacijenataSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/NarudzbePacijenataSQLImpl.java
@@ -4,7 +4,9 @@ import ba.unsa.etf.rpr.domain.NarudzbePacijenata;
 import ba.unsa.etf.rpr.exceptions.SmartDentistException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class NarudzbePacijenataSQLImpl extends AbstractDao<NarudzbePacijenata> implements NarudzbePacijenataDao{
 
@@ -30,11 +32,18 @@ public class NarudzbePacijenataSQLImpl extends AbstractDao<NarudzbePacijenata> i
 
     @Override
     public NarudzbePacijenata row2object(ResultSet rs) throws SmartDentistException {
-        return null;
+        try {
+            NarudzbePacijenata narudzbe = new NarudzbePacijenata();
+            narudzbe.setId(rs.getInt("id"));
+            narudzbe.setIme(rs.getString("ime"));
+            narudzbe.setPrezime(rs.getString("prezime"));
+            narudzbe.setDatum(rs.getDate("datum"));
+            narudzbe.setPovod(rs.getString("povod"));
+            return narudzbe;
+        } catch (SQLException e){
+            throw new SmartDentistException(e.getMessage(),e);
+        }
     }
 
-    @Override
-    public Map<String, Object> object2row(NarudzbePacijenata object) {
-        return null;
-    }
+
 }
