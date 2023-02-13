e71e8cb4177ea3c20ba324a2beed8b415da4ef91
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-13 18:45:44 +0100

    openDialog method impl. rasporedPacijenataOnClick method impl.

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class HomeController {
+    public void rasporedPacijenataOnClick(ActionEvent actionEvent) {
+        openDialog("Raspored pacijenata","/fxml/raspored_main.fxml",new RasporedMainController());
+    }
+
+    public void skladisteMaterijalaOnClick(ActionEvent actionEvent) {
+    }
+
+    private void openDialog(String title, String file, Object controller){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+            loader.setController(controller);
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setTitle(title);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.setResizable(false);
+            stage.show();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
 }
