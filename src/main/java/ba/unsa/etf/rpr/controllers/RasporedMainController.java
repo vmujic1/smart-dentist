f1ab10da3777e9b975b690611906dea5c3fdd090
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-14 15:26:55 +0100

    RasporedMainController initialize method impl

--- src/main/java/ba/unsa/etf/rpr/controllers/RasporedMainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RasporedMainController.java
@@ -3,12 +3,14 @@ package ba.unsa.etf.rpr.controllers;
 import ba.unsa.etf.rpr.business.RasporedManager;
 import ba.unsa.etf.rpr.domain.NarudzbePacijenata;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -26,6 +28,19 @@ public class RasporedMainController {
     public TableColumn<NarudzbePacijenata, Date> datumKolonaId;
 
     RasporedManager rasporedManager = new RasporedManager();
+    @FXML
+    public void initialize(){
+        idKolonaId.setCellValueFactory(new PropertyValueFactory<NarudzbePacijenata,Integer>("id"));
+        imeKolonaId.setCellValueFactory(new PropertyValueFactory<NarudzbePacijenata,String>("ime"));
+        prezimeKolonaId.setCellValueFactory(new PropertyValueFactory<NarudzbePacijenata,String>("prezime"));
+        opisKolonaId.setCellValueFactory(new PropertyValueFactory<NarudzbePacijenata,String>("povod"));
+        datumKolonaId.setCellValueFactory(new PropertyValueFactory<NarudzbePacijenata,Date>("datum"));
+        popuniTabelu();
+    }
+
+    private void popuniTabelu() {
+    }
+
 
     public void dodajOnClick(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader =  new FXMLLoader(getClass().getResource("/fxml/raspored_dodaj.fxml"));
