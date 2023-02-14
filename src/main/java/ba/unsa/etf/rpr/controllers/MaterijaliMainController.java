2acc77896178f7ffc78899f5ff1f21f083647d2a
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-14 22:50:14 +0100

    Methods added in MaterijaliMainController. MaterijaliManager added

--- src/main/java/ba/unsa/etf/rpr/controllers/MaterijaliMainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MaterijaliMainController.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MaterijaliMainController {
+    public TableView tabelaId;
+    public TableColumn idKolonaId;
+    public TableColumn imeKolonaId;
+    public TableColumn prezimeKolonaId;
+    public TextField pretragaId;
+
+
+
+    public void dodajuTabeluOnAction(ActionEvent actionEvent) {
+    }
+
+    public void urediIzTabeleAction(ActionEvent actionEvent) {
+    }
+
+    public void obrisiIzTabeleAction(ActionEvent actionEvent) {
+    }
+
+    public void odjaviSeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void rasporedPacijenataOnAction(ActionEvent actionEvent) {
+    }
 }
