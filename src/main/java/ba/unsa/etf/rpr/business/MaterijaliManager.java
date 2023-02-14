1c594222c1e344db7e58b20c013c5ee695cd721f
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-14 22:53:16 +0100

    MaterijaliManager getAll method added

--- src/main/java/ba/unsa/etf/rpr/business/MaterijaliManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/MaterijaliManager.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.Dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Materijali;
+import ba.unsa.etf.rpr.exceptions.SmartDentistException;
+
+import java.util.List;
+
 public class MaterijaliManager {
+    public static List<Materijali> getAll() throws SmartDentistException {
+        return DaoFactory.materijaliDao().getAll();
+    }
 }
