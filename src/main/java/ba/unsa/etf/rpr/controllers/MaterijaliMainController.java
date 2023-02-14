63b5849fc2348b9a7f169ce3139fa5ef86727f4b
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-14 23:05:19 +0100

    MaterijaliMainController initialize method impl

--- src/main/java/ba/unsa/etf/rpr/controllers/MaterijaliMainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MaterijaliMainController.java
@@ -1,17 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.domain.Materijali;
+import ba.unsa.etf.rpr.domain.NarudzbePacijenata;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Date;
 
 public class MaterijaliMainController {
     public TableView tabelaId;
     public TableColumn idKolonaId;
     public TableColumn imeKolonaId;
-    public TableColumn prezimeKolonaId;
+
     public TextField pretragaId;
+    public TableColumn kolicinaKolonaId;
+
+    @FXML
+    public void initialize(){
+        idKolonaId.setCellValueFactory(new PropertyValueFactory<Materijali,Integer>("id"));
+        imeKolonaId.setCellValueFactory(new PropertyValueFactory<Materijali,String>("naziv"));
+        kolicinaKolonaId.setCellValueFactory(new PropertyValueFactory<NarudzbePacijenata,String>("količina"));
+        popuniTabelu();
+    }
 
 
 
