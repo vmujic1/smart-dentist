94f38b3b0a28d29765f99d4e4ce9ec7164bf8d2f
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-14 23:30:00 +0100

    MaterijaliMainController dodajOnClick impl

--- src/main/java/ba/unsa/etf/rpr/controllers/MaterijaliMainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MaterijaliMainController.java
@@ -9,12 +9,14 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.util.Date;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
@@ -63,7 +65,14 @@ public class MaterijaliMainController {
 
 
 
-    public void dodajuTabeluOnAction(ActionEvent actionEvent) {
+    public void dodajuTabeluOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader =  new FXMLLoader(getClass().getResource("/fxml/materijali_dodaj.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.setResizable(false);
+        stage.show();
+        popuniTabelu();
     }
 
     public void urediIzTabeleAction(ActionEvent actionEvent) {
