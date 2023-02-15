2888a90726b7c8a1061a3741aded4b3f473d3362
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-15 23:17:11 +0100

    materijaliGetAllTest added

--- src/test/java/ba/unsa/etf/rpr/MaterijaliManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/MaterijaliManagerTest.java
@@ -1,11 +1,42 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.business.MaterijaliManager;
+import ba.unsa.etf.rpr.domain.Materijali;
+import ba.unsa.etf.rpr.exceptions.SmartDentistException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 /**
- * Class that provides test for MaterijaliManager
+ * Class that provides tests for MaterijaliManager
  * @author vmujic1
  */
 
 
 public class MaterijaliManagerTest {
+    MaterijaliManager materijaliManager = new MaterijaliManager();
+    @Test
+
+    void materijaliGetAllTest() throws SmartDentistException {
+        List<Materijali> lista = MaterijaliManager.getAll();
+        Materijali m = new Materijali();
+        m.setNaziv("Proba");
+        m.setKoličina(Integer.parseInt("12"));
+        materijaliManager.add(m);
+        List<Materijali> lista1 = MaterijaliManager.getAll();
+        materijaliManager.delete(m.getId());
+        boolean kontrola = false;
+        if(lista.size() + 1 == lista1.size()) kontrola = true;
+
+        Assertions.assertTrue(kontrola);
+
+
+
+    }
+
+
+
+
 
 }
