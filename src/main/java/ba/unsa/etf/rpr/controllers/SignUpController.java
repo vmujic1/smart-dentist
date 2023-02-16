e1bd9ed2ef9f9f97f276963bd7153f9d3c65e806
vmujic1 <92300059+vmujic1@users.noreply.github.com>
2023-02-16 02:32:31 +0100

    SignUpController fix

--- src/main/java/ba/unsa/etf/rpr/controllers/SignUpController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignUpController.java
@@ -5,15 +5,21 @@ import ba.unsa.etf.rpr.business.UserManager;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.SmartDentistException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.List;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class SignUpController {
     private final UserManager userManager = new UserManager();
@@ -99,5 +105,24 @@ public class SignUpController {
     }
 
     public void odustaniOnClick(MouseEvent mouseEvent) {
+        Stage s = (Stage)kontaktTelefonId.getScene().getWindow();
+        openDialog("Prijava","/fxml/login.fxml",null);
+        s.close();
+    }
+
+    private void openDialog(String title, String file, Object controller){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+            loader.setController(controller);
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setTitle(title);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.setResizable(false);
+            stage.show();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+
     }
 }
